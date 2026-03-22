710871fa0163f383496a26c68ade3c05742cca12
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-22 19:22:26 +0100

    Add User.js model with content

--- models/User.js
+++ models/User.js
@@ -1,1 +1,16 @@
-#public/models/User.j
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+	}tpection: { type: String, required: true },
+    email: { type: String, unique: true },
+    username: { type: String, required: true },
+    balance: { type: Number, default: 5 },
+    isPro: { type: Boolean, default: false },
+    currentLevel: { type: Number, default: 1 },
+    completedLevels: [{ type: Number }],
+    team: { type: mongoose.Schema.Types.ObjectId, ref:'Team' },
+    gasTaxPaid: { type: Number, default: 0 },
+    createAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
