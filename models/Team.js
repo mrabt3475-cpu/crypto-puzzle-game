8e08e7e9298b612f224b353e033da991728efc6d
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-22 19:22:27 +0100

    Add Team.js model with content

--- models/Team.js
+++ models/Team.js
@@ -1,1 +1,12 @@
-#public/models/Team.j
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+	name: {type: String, required: true},
+    leader: { type: mongoose.Schema.Types.ObjectId, ref:'Uper' },
+    members: [{type: ObjectId, ref:'Uper'}],
+    totalBalance: {type: Number, default: 0},
+    currentLevel: { type: Number, default: 1 },
+    createAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Team', teamSchema);
